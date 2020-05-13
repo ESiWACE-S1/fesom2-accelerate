9b7517ee4154c560528cc3d6e1fb538c9fa47d5e
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2020-05-13 16:06:05 +0200

    First stub of GPU code completed.

--- kernels/fct_ale_a3.py
+++ kernels/fct_ale_a3.py
@@ -4,7 +4,61 @@ import numpy
 import argparse
 
 def generate_code(tuning_parameters):
-    pass
+    numpy_real_type = None
+    if tuning_parameters["real_type"] == "float":
+        numpy_real_type = numpy.float32
+    elif tuning_parameters["real_type"] == "double":
+        numpy_real_type = numpy.float64
+    else:
+        raise ValueError
+    code = \
+        "__global__ void fct_ale_a3(const int maxLevels, const int maxElements, const int * __restrict__ nLevels, const int * __restrict__ elements_in_node, const int * __restrict__ number_elements_in_node, const <%REAL_TYPE%><%VECTOR_SIZE%> * __restrict__ UV_rhs, <%REAL_TYPE%> * __restrict__ fct_ttf_max, <%REAL_TYPE%> * __restrict__ fct_ttf_min, const <%REAL_TYPE%> * __restrict__ fct_lo)\n" \
+        "{\n" \
+        "<%INT_TYPE%> item = 0;\n" \
+        "extern __shared__ <%REAL_TYPE%> tvert_max[];\n" \
+        "extern __shared__ <%REAL_TYPE%> tvert_min[];\n" \
+        "/* Compute tvert_max and tvert_min per level */\n" \
+        "for ( <%INT_TYPE%> level = threadIdx.x; level < nLevels[blockIdx.x]; level += <%BLOCK_SIZE%> )\n" \
+        "{\n" \
+        "<%REAL_TYPE%> tvert_max_temp = 0.0;" \
+        "<%REAL_TYPE%> tvert_min_temp = 0.0;" \
+        "<%REDUCTION%>" \
+        "}\n" \
+        "__syncthreads();\n" \
+        "/* Update fct_ttf_max and fct_ttf_min per level */\n" \
+        "item = blockIdx.x * maxLevels;\n" \
+        "for ( <%INT_TYPE%> level = threadIdx.x + 1; level < nLevels[blockIdx.x] - 2; level += <%BLOCK_SIZE%> )\n" \
+        "{\n" \
+        "<%REAL_TYPE%> temp = 0.0;" \
+        "<%UPDATE%>" \
+        "}\n" \
+        "if ( threadIdx.x == 0 )\n" \
+        "{\n" \
+        "fct_ttf_max[item] = tvert_max[0] - fct_lo[item];\n" \
+        "fct_ttf_min[item] = tvert_min[0] - fct_lo[item];\n" \
+        "fct_ttf_max[item + (nLevels[blockIdx.x] - 1)] = tvert_max[nLevels[blockIdx.x] - 1] - fct_lo[item + (nLevels[blockIdx.x] - 1)];\n" \
+        "fct_ttf_min[item + (nLevels[blockIdx.x] - 1)] = tvert_min[nLevels[blockIdx.x] - 1] - fct_lo[item + (nLevels[blockIdx.x] - 1)];\n" \
+        "}\n" \
+        "}\n"
+    reduction = \
+        "item = (elements_in_node[(blockIdx.x * maxElements)] * maxLevels * 2) + ((level + <%OFFSET%>) * 2);\n" \
+        "tvert_max_temp = UV_rhs[item];\n" \
+        "tvert_min_temp = UV_rhs[item + 1];\n" \
+        "for ( <%INT_TYPE%> element = 1; element < number_elements_in_node[blockIdx.x]; element++ )\n" \
+        "{\n" \
+        "item = (elements_in_node[(blockIdx.x * maxElements) + element] * maxLevels * 2) + ((level + <%OFFSET%>) * 2);\n" \
+        "tvert_max_temp = fmax(tvert_max_temp, UV_rhs[item]);\n" \
+        "tvert_min_temp = fmin(tvert_min_temp, UV_rhs[item + 1]);\n" \
+        "}\n" \
+        "tvert_max[level + <%OFFSET%>] = tvert_max_temp;\n" \
+        "tvert_min[level + <%OFFSET%>] = tvert_min_temp;\n"
+    update = \
+        "temp = fmax(tvert_max[(level + <%OFFSET%>) - 1], tvert_max[level + <%OFFSET%>]);\n" \
+        "temp = fmax(temp, tvert_max[(level + <%OFFSET%>) + 1]);\n" \
+        "fct_ttf_max[item + level + <%OFFSET%>] = temp - fct_lo[item + level + <%OFFSET%>];\n" \
+        "temp = fmin(tvert_min[(level + <%OFFSET%>) - 1], tvert_min[level + <%OFFSET%>]);\n" \
+        "temp = fmin(temp, tvert_min[(level + <%OFFSET%>) + 1]);\n" \
+        "fct_ttf_min[item + level + <%OFFSET%>] = temp - fct_lo[item + level + <%OFFSET%>];\n"
 
 def reference(vlimit, nodes, levels, max_levels, elements_in_node, number_elements_in_node, max_elements_in_node, uv_rhs, fct_ttf_max, fct_ttf_min, fct_lo, real_type):
     tvert_max = 0
