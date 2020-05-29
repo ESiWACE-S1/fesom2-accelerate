964aa6d7eb835cd92cbdf81132045c31b84193c9
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2020-05-29 14:02:05 +0200

    Added shared memory option.

--- kernels/fct_ale_b1_vertical.py
+++ kernels/fct_ale_b1_vertical.py
@@ -45,6 +45,50 @@ def generate_code(tuning_parameters):
     code = code.replace("<%REAL_TYPE%>", tuning_parameters["real_type"])
     return code
 
+def generate_code_shared(tuning_parameters):
+    code = \
+        "__global__ void fct_ale_b1_vertical(const int maxLevels, const int * __restrict__ nLevels, const <%REAL_TYPE%> * __restrict__ fct_adf_v, <%REAL_TYPE%> * __restrict__ fct_plus, <%REAL_TYPE%> * __restrict__ fct_minus)\n" \
+        "{\n" \
+        "const <%INT_TYPE%> node = (blockIdx.x * maxLevels);\n" \
+        "extern __shared__ <%REAL_TYPE%> fct_adf_v_local[];\n" \
+        "\n" \
+        "for ( <%INT_TYPE%> level = threadIdx.x; level < nLevels[blockIdx.x]; level++ )\n" \
+        "{\n" \
+        "fct_adf_v_local[level] = fct_adf_v[node + level];\n" \
+        "}\n" \
+        "__syncthreads();\n" \
+        "for ( <%INT_TYPE%> level = threadIdx.x; level < nLevels[blockIdx.x] - 1; level += <%BLOCK_SIZE%> )\n" \
+        "{\n" \
+        "<%COMPUTE_BLOCK%>" \
+        "}\n" \
+        "}\n"
+    compute_block = \
+        "fct_plus[node + level + <%OFFSET%>] = <%FMAX%>(0.0, fct_adf_v_local[level + <%OFFSET%>]) + <%FMAX%>(0.0, -fct_adf_v_local[level + <%OFFSET%> + 1]);\n" \
+        "fct_minus[node + level + <%OFFSET%>] = <%FMIN%>(0.0, fct_adf_v_local[level + <%OFFSET%>]) + <%FMIN%>(0.0, -fct_adf_v_local[level + <%OFFSET%> + 1]);\n"
+    if tuning_parameters["tiling_x"] > 1:
+        code = code.replace("<%BLOCK_SIZE%>", str(tuning_parameters["block_size_x"] * tuning_parameters["tiling_x"]))
+    else:
+        code = code.replace("<%BLOCK_SIZE%>", str(tuning_parameters["block_size_x"]))
+    compute = str()
+    for tile in range(0, tuning_parameters["tiling_x"]):
+        if tile == 0:
+            compute = compute + compute_block.replace(" + <%OFFSET%>", "")
+        else:
+            offset = tuning_parameters["block_size_x"] * tile
+            compute = compute + "if (level + {} < nLevels[blockIdx.x] - 1)\n{{\n{}}}\n".format(str(offset), compute_block.replace("<%OFFSET%>", str(offset)))
+    code = code.replace("<%COMPUTE_BLOCK%>", compute)
+    if tuning_parameters["real_type"] == "float":
+        code = code.replace("<%FMAX%>", "fmaxf")
+        code = code.replace("<%FMIN%>", "fminf")
+    elif tuning_parameters["real_type"] == "double":
+        code = code.replace("<%FMAX%>", "fmax")
+        code = code.replace("<%FMIN%>", "fmin")
+    else:
+        raise ValueError
+    code = code.replace("<%INT_TYPE%>", tuning_parameters["int_type"].replace("_", " "))
+    code = code.replace("<%REAL_TYPE%>", tuning_parameters["real_type"])
+    return code
+
 def reference(nodes, levels, max_levels, fct_adf_v, fct_plus, fct_minus):
     for node in range(0, nodes):
         for level in range(0, levels[node] - 1):
@@ -57,7 +101,7 @@ def reference(nodes, levels, max_levels, fct_adf_v, fct_plus, fct_minus):
             fct_plus[item] = (max(0.0, fct_adf_v[item]) + max(0.0, -fct_adf_v[(node * max_levels) + level + 1]))
             fct_minus[item] = (min(0.0, fct_adf_v[item]) + min(0.0, -fct_adf_v[(node * max_levels) + level + 1]))
 
-def tune(nodes, max_levels, max_tile, real_type):
+def tune(nodes, max_levels, max_tile, shared_memory, real_type):
     numpy_real_type = None
     if real_type == "float":
         numpy_real_type = numpy.float32
@@ -67,11 +111,15 @@ def tune(nodes, max_levels, max_tile, real_type):
         raise ValueError
     # Tuning and code generation parameters
     tuning_parameters = dict()
+    tuning_parameters["shared_memory"] = [shared_memory]
     tuning_parameters["int_type"] = ["unsigned_int", "int"]
     tuning_parameters["real_type"] = [real_type]
     tuning_parameters["max_levels"] = [str(max_levels)]
     tuning_parameters["block_size_x"] = [32 * i for i in range(1, 33)]
     tuning_parameters["tiling_x"] = [i for i in range(1, max_tile)]
+    shared_memory_args = dict()
+    if shared_memory:
+        shared_memory_args["size"] = max_levels * numpy.dtype(numpy_real_type).itemsize
     constraints = list()
     constraints.append("block_size_x * tiling_x <= max_levels")
     # Memory allocation and initialization
@@ -88,7 +136,10 @@ def tune(nodes, max_levels, max_tile, real_type):
     reference(nodes, levels, max_levels, fct_adf_v, fct_plus_control, fct_minus_control)
     arguments_control = [None, None, None, fct_plus_control, fct_minus_control]
     # Tuning
-    results, environment = tune_kernel("fct_ale_b1_vertical", generate_code, "{} * block_size_x".format(nodes), arguments, tuning_parameters, lang="CUDA", answer=arguments_control, restrictions=constraints, quiet=True)
+    if shared_memory:
+        results, environment = tune_kernel("fct_ale_b1_vertical", generate_code, "{} * block_size_x".format(nodes), arguments, tuning_parameters, smem_args=shared_memory_args, lang="CUDA", answer=arguments_control, restrictions=constraints, quiet=True)
+    else:
+        results, environment = tune_kernel("fct_ale_b1_vertical", generate_code, "{} * block_size_x".format(nodes), arguments, tuning_parameters, lang="CUDA", answer=arguments_control, restrictions=constraints, quiet=True)
     return results
 
 def parse_command_line():
@@ -101,7 +152,8 @@ def parse_command_line():
 
 if __name__ == "__main__":
     command_line = parse_command_line()
-    results = tune(command_line.nodes, command_line.max_levels, command_line.max_tile, command_line.real_type)
+    results = tune(command_line.nodes, command_line.max_levels, command_line.max_tile, False, command_line.real_type)
+    results = results + tune(command_line.nodes, command_line.max_levels, command_line.max_tile, True, command_line.real_type)
     best_configuration = min(results, key=lambda x : x["time"])
     print("/* Block size X: {} */".format(best_configuration["block_size_x"]))
     print(generate_code(best_configuration))
