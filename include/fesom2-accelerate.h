ee91d349558ec6ea9d9af49307c4e0d2dcd934ed
gvdoord <g.vandenOord@gmail.com>
2020-06-05 10:01:00 +0200

    Fixed transfer_async in header

--- include/fesom2-accelerate.h
+++ include/fesom2-accelerate.h
@@ -121,7 +121,7 @@ void fct_ale_a1_reference_( int * nNodes, int * nLevels_nod2D, int * maxLevels_p
 void transfer_mesh_(void** ret, int* host_ptr, int* size, int* istat);
 void alloc_var_(void** ret, real_type* host_ptr, int* size, int* istat);
 void reserve_var_(void** ret, int* size, int* istat);
-void transfer_var_async_(void** mem, real_type* host_ptr, int* istat);
+void transfer_var_async_(void** mem, real_type* host_ptr);
 
 void fct_ale_pre_comm_acc_( int* alg_state, void** fct_ttf_max, void**  fct_ttf_min, void**  fct_plus, void**  fct_minus, void** ttf, void** fct_LO, void**  fct_adf_v, void** fct_adf_h, void** UV_rhs, real_type* area_inv, int* myDim_nod2D, int* eDim_nod2D, int* myDim_elem2D, int* myDim_edge2D, int* nl, void** nlevels_nod2D, void** nlevels_elem2D, void** elem2D_nodes, void** nod_in_elem2D_num, void** nod_in_elem2D, int* nod_in_elem2D_dim, void** nod2D_edges, void** elem2D_edges, int* vlimit, real_type* flux_eps, real_type* bignumber, real_type* dt);
 /**
