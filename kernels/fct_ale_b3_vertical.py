3e8f337a7783c3b84e2ec5806403e1bd45ee144d
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2020-08-06 14:41:21 +0200

    Forgot a factor in shared memory allocation.

--- kernels/fct_ale_b3_vertical.py
+++ kernels/fct_ale_b3_vertical.py
@@ -221,7 +221,7 @@ def tune(nodes, max_levels, max_tile, real_type, quiet=True):
     # Shared memory version
     shared_memory_args = dict()
     tuning_parameters["shared_memory"] = [True]
-    shared_memory_args["size"] = max_levels * numpy.dtype(numpy_real_type).itemsize
+    shared_memory_args["size"] = max_levels * 2 * numpy.dtype(numpy_real_type).itemsize
     arguments = [numpy.int32(max_levels), levels, fct_adf_v_shared, fct_plus, fct_minus]
     results_shared, _ = tune_kernel("fct_ale_b3_vertical", generate_code_shared, "{} * block_size_x".format(nodes), arguments, tuning_parameters, smem_args=shared_memory_args, lang="CUDA", answer=arguments_control, restrictions=constraints, quiet=quiet)
     # Memory bandwidth shared memory version
